7d629bd5603155a70fab04fadd2453004d813e82
Andrew Vos <andrew.vos@gmail.com>
2021-04-06 19:27:24 +0100

    Add favicons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import classNames from "classnames";
 import emotions from "./emotions.js";
+import Head from "next/head";
 
 const SimpleStep = ({ children, onMoveToNextStepRequest }) => {
   return (
@@ -239,5 +240,31 @@ export default function Home() {
     </SimpleStep>,
   ];
 
-  return steps[stepIndex];
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      {steps[stepIndex]}
+    </>
+  );
 }
